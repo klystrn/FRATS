342fb028ff7573947de7b8eae80f695bb4256753
yqy09091 <yeqiyangasd@gmail.com>
2023-12-24 17:03:36 +0800

    Commit 4.1 1:edit all html page 2:remove user.db 3:change FRATS.db datase from 1 to 2 4:edit main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
@@ -22,10 +22,9 @@ class UploadFileForm(FlaskForm):
     file = FileField("File")
     Submit = SubmitField("Upload File")
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
+@app.route('/webcam', methods=['GET',"POST"])
 
-def home():
+def webcam():
     form = UploadFileForm()
     if form.validate_on_submit():
         # First grab the file
@@ -35,13 +34,17 @@ def home():
     # Data will be available from POST submitted by the form
     if request.method == 'POST':
         try:
-            nm = request.form['nm']
+            #nm = request.form['nm']
             img = file.read()
+            username = session["UserName"]
             # Connect to SQLite3 database and execute the INSERT
             cur = con.cursor()
-            cur.execute("SELECT MAX(ID) FROM Userinfo LIMIT 1")
-            ID = cur.fetchone()[0]
-            cur.execute("INSERT INTO Userinfo  (ID, Name, Image, DateTimeSaved) VALUES (?, ?, ?, ?)",(ID+1, nm, img, now))
+            #cur.execute("SELECT MAX(ID) FROM Userinfo LIMIT 1")
+            #ID = cur.fetchone()[0]
+            #if (ID is None):
+            #   ID = 0
+            
+            cur.execute("INSERT INTO Image  (UserName, Image, DateTimeSaved) VALUES (?, ?, ?)",( username, img, now))
             con.commit()
             msg = "Record successfully added to database"
             
@@ -57,6 +60,7 @@ def sp():
         username = request.form['uname']
         password = request.form['npw']
         email = request.form['uemail']
+        name = request.form['name']
 
         # Validate the user's input
         if not username or not password or not email:
@@ -65,9 +69,9 @@ def sp():
 
         # Create a new user account in the database
         try:
-            conn = sqlite3.connect('user.db')
+            conn = sqlite3.connect('FRATS.db')
             cur = conn.cursor()
-            cur.execute("INSERT INTO RegUser (UserName, Password, Email) VALUES (?, ?, ?)", (username, password, email))
+            cur.execute("INSERT INTO RegUser (UserName, Password, Email, RealName) VALUES (?, ?, ?, ?)", (username, password, email, name))
             conn.commit()
             conn.close()
             success = "Account created successfully."
@@ -84,7 +88,7 @@ def log():
         username = request.form['una']
         password = request.form['upass']
 
-        connection = sqlite3.connect('user.db')
+        connection = sqlite3.connect('FRATS.db')
         cursor = connection.cursor()
 
         cursor.execute('SELECT * FROM RegUser WHERE UserName = ?', (username,))
@@ -93,7 +97,8 @@ def log():
 
         if user:
             if user[1] == password:
-                return redirect(url_for('home'))
+                session["UserName"] = username
+                return redirect(url_for('webcam'))
             else:
                 error = 'Invalid password'
         else:
@@ -104,6 +109,8 @@ def log():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/')
 
 @app.route('/index')
 def index():
@@ -124,3 +131,4 @@ def signup():
 if __name__ == '__main__':
     app.run(debug=True)
 
+#session.pop("UserName",default=None) for log out

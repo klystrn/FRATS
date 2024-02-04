d528521b07fc8b7f37145517a4131c9ae4b90592
Jai-29 <S10244397@connect.np.edu.sg>
2024-02-04 23:24:37 +0800

    Commit 5.1.4.1
    Created a new set of html page

--- main.py
+++ main.py
@@ -172,6 +172,10 @@ def card_id():
 def view():
     return render_template('view.html')
 
+@app.route('/view1')
+def view1():
+    return render_template('view1.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
